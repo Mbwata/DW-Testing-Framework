66e9a5a6a50502068b31eadb58c849bc602fc97f
Mbwata <housems@hotmail.com>
2019-05-06 14:26:22 -0500

    Update Tables.sql

--- Users/Tables.sql
+++ Users/Tables.sql
@@ -2,24 +2,22 @@
 --  File created - Monday-May-06-2019   
 --------------------------------------------------------
 --------------------------------------------------------
---  DDL for Table ORDER_HEADER
---------------------------------------------------------
-
-  CREATE TABLE "DUMMY"."ORDER_HEADER" 
-   (	"ORDER_KEY" NUMBER, 
-	"ORDER_ID" VARCHAR2(20 BYTE), 
-	"CUSTOMER_KEY" NUMBER, 
-	"ORDER_DATE" DATE
-   ) SEGMENT CREATION DEFERRED 
-  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
- NOCOMPRESS LOGGING
-  TABLESPACE "USERS" ;
---------------------------------------------------------
 --  DDL for Table ORDER_LINES
 --------------------------------------------------------
 
   CREATE TABLE "DUMMY"."ORDER_LINES" 
    (	"LINE_KEY" NUMBER
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  DDL for Table ORDER_XREF
+--------------------------------------------------------
+
+  CREATE TABLE "DUMMY"."ORDER_XREF" 
+   (	"ORDER_KEY" NUMBER, 
+	"ORDER_ID" VARCHAR2(20 BYTE)
    ) SEGMENT CREATION DEFERRED 
   PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
  NOCOMPRESS LOGGING
@@ -32,6 +30,22 @@
    (	"ORDER_ID" VARCHAR2(20 BYTE), 
 	"CUSTOMER_KEY" VARCHAR2(20 BYTE), 
 	"ORDER_DATE" VARCHAR2(20 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  DDL for Table STAGE_ORDER_HEADER
+--------------------------------------------------------
+
+  CREATE TABLE "DUMMY"."STAGE_ORDER_HEADER" 
+   (	"ORDER_KEY" NUMBER, 
+	"ORDER_ID" VARCHAR2(20 BYTE), 
+	"CUSTOMER_KEY" NUMBER, 
+	"ORDER_DATE" DATE
    ) SEGMENT CREATION DEFERRED 
   PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
  NOCOMPRESS LOGGING
